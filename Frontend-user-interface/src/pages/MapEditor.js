a7e56a340f7995c86e04c42295cac5b963d2f460
jclaerhout <j.claerhout@students.ephec.be>
2023-11-22 17:56:27 +0100

    Enhanced Drawing App: Shapes, Resize, and Save Functionality
    Shape Drawing: Users can now draw rectangles and circles, adjusting their size dynamically.
    
    Save Functionality:
    A new "Save" button allows users to save the current canvas, including any drawn shapes, as a PNG file.
    This feature lays the groundwork for future integration with an API route to upload the canvas to an Amazon S3 bucket.
    Note that the backend implementation for this functionality is pending.
    
    Please note that the backend implementation for uploading to Amazon S3 will be addressed in subsequent commits.

--- Frontend-user-interface/src/pages/MapEditor.js
+++ Frontend-user-interface/src/pages/MapEditor.js
@@ -2,10 +2,14 @@ import "../styles/MapEditor.css";
 
 import React, { useState, useRef } from 'react'
 import { useLocation } from 'react-router-dom';
-import { Stage, Layer, Rect, Line, Circle, Image as KonvaImage, Text } from 'react-konva';
+import { Stage, Layer, Rect, Line, Circle, Image as KonvaImage } from 'react-konva';
 import useImage from 'use-image';
-import { Box, Radio, RadioGroup, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem,
-    Select, Typography, Slider, Grid } from "@mui/material";
+import { Box, Radio, RadioGroup, FormControl, FormControlLabel, InputLabel, MenuItem,
+    Select, Typography, Slider, Grid, Button } from "@mui/material";
+import DrawIcon from '@mui/icons-material/Draw';
+import InterestsIcon from '@mui/icons-material/Interests';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from "@mui/material";
 
 const useMapForKonva = (map) => {
     const [image] = useImage(map);
@@ -16,14 +20,21 @@ const MapEditor = () => {
     const { state } = useLocation();
     const map = useMapForKonva(state);
 
-    const [stageSize, setStageSize] = useState("800x800");
+    const [stageSize, setStageSize] = useState("500x500");
+
+    const [open, setOpen] = useState(false);
+    const [mapName, setMapName] = useState('');
 
     const [tool, setTool] = useState('pen');
     const [lines, setLines] = useState([]);
+    const [shapes, setShapes] = useState([]);
+    const [shapeType, setShapeType] = useState('');
+    const [drawingShape, setDrawingShape] = useState(null);
     const [lineWidth, setLineWidth] = useState(2);
     const [eraserWidth, setEraserWidth] = useState(2);
 
     const isDrawing = useRef(false);
+    const layerRef = useRef(null);
 
     // Convertir la taille de la scène en un objet lorsque vous en avez besoin
     const stageSizeObject = {
@@ -35,26 +46,87 @@ const MapEditor = () => {
         setStageSize(event.target.value);
     };
 
+    const handleShapeClick = (index) => {
+        if (tool === 'eraser' && isDrawing.current) {
+            setShapes(prevShapes => prevShapes.filter((shape, i) => i !== index));
+        }
+    };
+
+    const saveAsPng = () => {
+        // Obtenez une référence à votre scène Konva
+        const stage = layerRef.current.getStage();
+        // Convertissez le contenu de la scène en une URL de données PNG
+        const dataUrl = stage.toDataURL();
+        // Créez un nouvel élément de lien
+        const link = document.createElement('a');
+        // Définissez l'URL de données comme href du lien
+        link.href = dataUrl;
+        // Définissez l'attribut de téléchargement du lien pour définir le nom du fichier de l'image téléchargée
+        link.download = mapName + '.png';
+        // Déclenchez un clic sur le lien pour commencer le téléchargement
+        link.click();
+      };
+
     const handleMouseDown = (e) => {
-        isDrawing.current = true;
-        const pos = e.target.getStage().getPointerPosition();
-        setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+        const point = e.target.getStage().getPointerPosition();
+
+        if (tool === 'pen' || tool === 'eraser') {
+
+            isDrawing.current = true;
+            setLines([...lines, { tool, points: [point.x, point.y] }])
+        } else if (tool === 'shape') {
+
+            isDrawing.current = true;
+            let shape;
+            if (shapeType === 'rectangle') {
+                shape = { type: 'Rect', x: point.x, y: point.y, width: 0, height: 0, stroke: 'red' };
+            } else if (shapeType === 'circle') {
+                shape = { type: 'Circle', x: point.x, y: point.y, radius: 0, stroke: 'red' };
+            } else if (shapeType === 'straightLine') {
+                shape = { type: 'Line', points: [point.x, point.y, point.x, point.y], stroke: 'red' };
+            }
+        
+            if (shape) {
+            setDrawingShape(shape);
+            }
+        }
     };
     
     const handleMouseMove = (e) => {
+        // Ne pas dessiner si on n'est pas en mode dessin
         if (!isDrawing.current) return;
+        const point = e.target.getStage().getPointerPosition();
     
-        const stage = e.target.getStage();
-        const point = stage.getPointerPosition();
-        let lastLine = lines[lines.length - 1];
-        lastLine.points = lastLine.points.concat([point.x, point.y]);
+        // Dessiner en fonction de l'outil sélectionné
+        if (tool === 'pen' || tool === 'eraser') {
+            let lastLine = lines[lines.length - 1];
+            lastLine.points = lastLine.points.concat([point.x, point.y]);
     
-        lines.splice(lines.length - 1, 1, lastLine);
-        setLines(lines.concat());
+            lines.splice(lines.length - 1, 1, lastLine);
+            setLines(lines.concat());
+        } else if (tool === 'shape') {
+            // Dessiner la forme sélectionnée
+            let shape = { ...drawingShape };
+
+            if (shapeType === 'rectangle') {
+                shape.width = point.x - shape.x;
+                shape.height = point.y - shape.y;
+            } else if (shapeType === 'circle') {
+                shape.radius = Math.sqrt(Math.pow(point.x - shape.x, 2) + Math.pow(point.y - shape.y, 2));
+            } else if (shapeType === 'straightLine') {
+                shape.points = [shape.points[0], shape.points[1], point.x, point.y];
+            }
+          
+            setDrawingShape(shape);
+        }
     };
     
     const handleMouseUp = () => {
         isDrawing.current = false;
+        if (drawingShape) {
+          setShapes(prevShapes => [...prevShapes, drawingShape]);
+          setDrawingShape(null);
+        }
     };
 
     return (
@@ -72,9 +144,9 @@ const MapEditor = () => {
                             label="Taille de carte"
                             onChange={handleSizeChange}
                         >
-                            <MenuItem value="1000x1000">Grande</MenuItem>
-                            <MenuItem value="800x800">Moyenne</MenuItem>
                             <MenuItem value="500x500">Petite</MenuItem>
+                            <MenuItem value="800x800">Moyenne</MenuItem>
+                            <MenuItem value="1000x1000">Grande</MenuItem>
                         </Select>
                     </FormControl>
                 </Box>
@@ -83,7 +155,7 @@ const MapEditor = () => {
                         <RadioGroup value={tool} onChange={(e) => { setTool(prevTool => prevTool === e.target.value ? null : e.target.value); }}>
                             <Grid container spacing={2}>
                                 <Grid item xs={6}>
-                                    <FormControlLabel value="pen" control={<Radio />} label="Pinceau" />
+                                    <FormControlLabel value="pen" control={<Radio />} label={<><span>Pinceau </span><DrawIcon /></>} />
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Box sx={{ width: 300 }}>
@@ -93,8 +165,31 @@ const MapEditor = () => {
                                         <Slider valueLabelDisplay="auto" min={0} max={10} value={lineWidth} onChange={(e, newValue) => setLineWidth(newValue)}/>
                                     </Box>
                                 </Grid>
+
                                 <Grid item xs={6}>
-                                    <FormControlLabel value="eraser" control={<Radio />} label="Gomme" />
+                                    <FormControlLabel value="shape" control={<Radio />} label={<><span>Formes </span><InterestsIcon /></>} />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Box sx={{ width: 300 }}>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="shape-select-label">Choix de la forme</InputLabel>
+                                        <Select
+                                            labelId="shape-select-label"
+                                            id="shape-select"
+                                            value={shapeType}
+                                            label="Choix de la forme"
+                                            onChange={(e) => setShapeType(e.target.value)}
+                                        >
+                                            <MenuItem value="rectangle">Rectangle</MenuItem>
+                                            <MenuItem value="circle">Cercle</MenuItem>
+                                            <MenuItem value="straightLine">Ligne droite</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    </Box>
+                                </Grid>
+
+                                <Grid item xs={6}>
+                                    <FormControlLabel value="eraser" control={<Radio />} label={<><span>Gomme </span><BackspaceIcon /></>} />
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Box sx={{ width: 300 }}>
@@ -107,6 +202,36 @@ const MapEditor = () => {
                             </Grid>
                         </RadioGroup>
                     </FormControl>
+                </Box>
+                <Box className="map-save-button">
+                    <Button onClick={() => setOpen(true)} variant="contained">
+                        Enregister
+                    </Button>
+                    <Dialog open={open} onClose={() => setOpen(false)}>
+                        <DialogTitle>Enregistrer la carte</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Veuillez entrer le nom de la carte.
+                            </DialogContentText>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                label="Nom de la carte"
+                                type="text"
+                                fullWidth
+                                value={mapName}
+                                onChange={e => setMapName(e.target.value)}
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={() => setOpen(false)}>
+                                Annuler
+                            </Button>
+                            <Button onClick={saveAsPng}>
+                                Enregistrer
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </Box>
             </div>
             <Stage className="konva" width={stageSizeObject.width} height={stageSizeObject.height}
@@ -125,11 +250,22 @@ const MapEditor = () => {
                         strokeWidth={1}
                     />
                 </Layer>
-                <Layer>
+                <Layer ref={layerRef}>
                     {/* Dessin en fonction de l'outil sélectionné */}
                     {lines.map((line, i) => (
                         <Line key={i} points={line.points} stroke="#df4b26" strokeWidth={ line.tool === 'eraser' ? eraserWidth : lineWidth } tension={0.5} lineCap="round" globalCompositeOperation={ line.tool === 'eraser' ? 'destination-out' : 'source-over' } />
                     ))}
+                    {shapes.map((shape, i) => {
+                        if (shape.type === 'Rect') {
+                            return <Rect key={i} {...shape} onMouseEnter={() => handleShapeClick(i)} />;
+                        } else if (shape.type === 'Circle') {
+                            return <Circle key={i} {...shape} onMouseEnter={() => handleShapeClick(i)} />;
+                        } else if (shape.type === 'straightLine') {
+                            return <Line key={i} {...shape} onMouseEnter={() => handleShapeClick(i)} />;
+                        } else {
+                            return null
+                        }
+                    })}
                 </Layer>
             </Stage>
         </div>
