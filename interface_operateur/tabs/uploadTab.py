ce4775e04abf463d2c2f4a049d560cca3d551101
Slywoud <he202160@students.ephec.be>
2023-11-29 18:21:03 +0100

    Last adjustments for uploadTab.py and creation of test_upload_tab.py
    Latest adjustments to uploadTab.py and addition of unit tests with test_upload_tab.py

--- interface_operateur/tabs/uploadTab.py
+++ interface_operateur/tabs/uploadTab.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import os
 from tkinter import *
 from tkinter.ttk import *
@@ -5,31 +6,34 @@ from tkinter import filedialog
 from tkcalendar import DateEntry
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from google.cloud.firestore_v1.base_query import FieldFilter
+from google.cloud.firestore_v1.client import Client
 import boto3
 from dotenv import load_dotenv
 
-#Needs to change
+# Load environment variables from the .env file
+load_dotenv()
+
+# Firebase Configuration
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 cred = credentials.Certificate('././keys/roommapping-group-5-firebase-adminsdk-oycah-ca3f0c23d8.json')
-
 app = firebase_admin.initialize_app(cred)
-
 db = firestore.client()
-#Needs to change
 
-load_dotenv()
-
+# S3 Configuration
 bucket_name = "roommappingbucket"
 region = "eu-north-1"
 access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
+# End of configuration
 
 class UploadTab:
     def __init__(self, tabControl):
+        self.db = firestore.client()
+        # Initialization of the tab and frames
         self.tab = Frame(master=tabControl)
         tabControl.add(self.tab, text="Upload")
 
@@ -38,6 +42,7 @@ class UploadTab:
         self.wrapper1.pack(side="left", fill="both", expand=TRUE, padx=10, pady=10)
         self.wrapper2.pack(side="left", fill="both", expand=TRUE, padx=10, pady=10)
 
+        # Definition of the form fields
         self.nom_label = Label(self.wrapper1, text="Nom:")
         self.nom_label.grid(row=0, column=0, padx=5, pady=5, sticky="e")
         self.nom_entry = Entry(self.wrapper1)
@@ -58,47 +63,59 @@ class UploadTab:
         self.date_entry = DateEntry(self.wrapper1)
         self.date_entry.grid(row=3, column=1, padx=5, pady=5)
 
+        # Button to upload the image
         self.upload_button = Button(self.wrapper1, text="Upload Image", command=self.upload_image)
         self.upload_button.grid(row=4, columnspan=2, pady=10)
 
+        # Label to display the image in frame 2
         self.image_label = Label(self.wrapper2)
         self.image_label.pack(side=RIGHT, padx=10)
 
+    # Method to upload an image
     def upload_image(self):
         file_path = filedialog.askopenfilename(title="Select a map",
                                                filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
 
         if file_path:
+            # Open and resize the image
             image = Image.open(file_path)
             image.thumbnail((150, 150))
             photo = ImageTk.PhotoImage(image)
+
+            # Configure the image in the canvas
             self.image_label.config(image=photo)
             self.image_label.image = photo
 
+            # Get form data
             nom = self.nom_entry.get()
             lieu = self.lieu_entry.get()
             mail = self.mail_entry.get()
             date = self.date_entry.get()
 
+            # Validate form fields
             if self.validate_fields(nom, lieu, date, mail):
+                # Get user ID
                 user_id = self.get_user_id(mail)
                 if user_id:
+                    # Upload the image to S3
                     self.upload_to_s3(file_path)
+                    # Add information to Firestore
                     self.upload_to_firestore(nom, lieu, date, file_path, mail)
 
-            return mail
-
+    # Method to validate form fields
     def validate_fields(self, nom, lieu, date, mail):
         if not nom or not lieu or not date or not mail:
-            messagebox.showerror("Champs manquants", "Veuillez remplir tous les champs.")
+            messagebox.showerror("Missing Fields", "Please fill in all fields.")
             return False
         return True
 
+    # Method to get the user ID from the email
     def get_user_id(self, mail):
-        users_collection = db.collection('users')
+        users_collection = self.db.collection('users')
         query = users_collection.where('email', '==', mail)
         results = query.get()
 
+        # Check if the email exists in Cloud Firestore and retrieve its ID
         for doc in results:
             user_id = doc.id
             print(f'Found user with email: {mail}, userId: {user_id}')
@@ -107,19 +124,23 @@ class UploadTab:
         print(f'No user found with email: {mail}')
         return None
 
+    # Method to upload the image to Amazon S3
     def upload_to_s3(self, file_path):
-        s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
         file_name = file_path.split("/")[-1]
-        s3.upload_file(file_path, bucket_name, file_name)
+        s3_client = boto3.client('s3', region_name=region, aws_access_key_id=access_key_id,
+                                 aws_secret_access_key=secret_access_key)
+        s3_client.upload_file(file_path, bucket_name, file_name)
 
         object_url = f"https://roommappingbucket.s3.eu-north-1.amazonaws.com/{file_name}"
         print(object_url)
 
         return object_url
 
+    # Method to send form information to Cloud Firestore
     def upload_to_firestore(self, nom, lieu, date, file_path, mail):
-        users_collection = db.collection('users')
+        users_collection = self.db.collection('users')
         user_id = self.get_user_id(mail)
+        object_url = self.upload_to_s3(file_path)
 
         if not user_id:
             print(f'No user found with email: {mail}')
@@ -129,7 +150,7 @@ class UploadTab:
             'nom': nom,
             'lieu': lieu,
             'date': date,
-            'url': file_path
+            'url': object_url
         })
 
         print("Document added to user")
