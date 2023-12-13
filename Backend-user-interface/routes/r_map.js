b9cad85381b708ff79992667001692f2d617433b
Slywoud <he202160@students.ephec.be>
2023-12-13 18:33:01 +0100

    Changes for a zoom functionnality.
    this branch is used for personnal use and will not be merged with the main branch. This commit permits me to keep changes made but not implemented for personnal use.

--- Backend-user-interface/routes/r_map.js
+++ Backend-user-interface/routes/r_map.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/:uid', userCtrl.getMaps)
+router.get('/:uid/:mapId', userCtrl.getMap)
 
 module.exports = router;
--- Frontend-user-interface/src/pages/Zoomzoom.js
+++ Frontend-user-interface/src/pages/Zoomzoom.js
@@ -1,33 +1,110 @@
-import React from "react";
-import {
-    TransformWrapper,
-    TransformComponent,
-    useControls
-} from "react-zoom-pan-pinch";
+import React, { useState, useEffect } from "react";
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { redirect } from "react-router-dom";
+const apiUrl = process.env.REACT_APP_API_URL;
 
-function App() {
+function Zoom() {
+    const [maps, setMaps] = useState([]);
+    const [selectedMap, setSelectedMap] = useState(null);
+    const [fetchData, setfetchData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const fetchMaps = (uid) => {
+        fetch(`${apiUrl}/api/map/${uid}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setIsLoaded(true);
+                setfetchData(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching maps:", error);
+            });
+    };
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
+                fetchMaps(uid);
+            } else {
+                redirect("/login");
+            }
+        });
+    }, []);
+
+    const handleDropdownChange = (event) => {
+        const selectedMapId = event.target.value;
+        const selectedMap = fetchData.find((map) => map.mapId === selectedMapId);
+        setSelectedMap(selectedMap);
+    };
+
     const Controls = () => {
         const { zoomIn, zoomOut, resetTransform } = useControls();
+
         return (
-            <>
-                <button onClick={() => zoomIn()}>Zoom In</button>
-                <button onClick={() => zoomOut()}>Zoom Out</button>
-                <button onClick={() => resetTransform()}>Reset</button>
-            </>
+            <div style={{ position: "absolute", bottom: "10px", left: "50%", transform: "translateX(-50%)" }}>
+                <Form>
+                    <Form.Select value={selectedMap ? selectedMap.mapId : "default"} onChange={handleDropdownChange}>
+                        <option value="default">Sélectionnez une Map</option>
+                        {fetchData.map((map) => (
+                            <option key={map.mapId} value={map.mapId}>
+                                {map.url.substring(map.url.lastIndexOf("/") + 1)}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form>
+                <Button variant="primary" onClick={() => zoomIn()}>
+                    Zoom In
+                </Button>{" "}
+                <Button variant="secondary" onClick={() => zoomOut()}>
+                    Zoom Out
+                </Button>{" "}
+                <Button variant="danger" onClick={() => resetTransform()}>
+                    Reset
+                </Button>
+            </div>
         );
     };
+
+    const containerStyle = {
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+    };
+
+    const imageStyle = {
+        width: "100%",
+        height: "auto",
+        objectFit: "contain",
+    };
+
     return (
-        <TransformWrapper>
-            <Controls />
-            <TransformComponent>
-                <img
-                    src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                    alt="test"
-                    width="100%"
-                />
-            </TransformComponent>
-        </TransformWrapper>
+        <div className="Zoomzoom" style={containerStyle}>
+            <TransformWrapper defaultScale={1}>
+                <TransformComponent>
+                    <img
+                        src={selectedMap ? selectedMap.url : ""}
+                        alt={selectedMap ? selectedMap.mapName : "Veuillez sélectionner une Map"}
+                        style={imageStyle}
+                    />
+                </TransformComponent>
+                <Controls />
+            </TransformWrapper>
+        </div>
     );
 }
 
-export default App;
+export default Zoom;
