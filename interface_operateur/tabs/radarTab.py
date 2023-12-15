ed0625fda3fb2d18ace2ff6b501b0d0c49628b95
Bonheure Antoine <he202146@students.ephec.be>
2023-12-15 09:14:10 +0100

    Impl Radar: Echelle modifiable du radar
    US-38 Créer une échelle modifiable sur le Radar afin d'être plus précis lors de la visualisation
    
    Cette fonctionnalité a pour but de modifier l'échelle du radar afin de visualiser un mieux les grands et les petites distances.
    Elle est initialiser pas un slider qui vous permet de changer la vue et l'échelle du radar qui correspond le mieux à l'envie.
    
    x

--- interface_operateur/tabs/radarTab.py
+++ interface_operateur/tabs/radarTab.py
@@ -13,12 +13,16 @@ import websockets
 import json
 import threading as th
 
+#Pour la fonction test
+#from .mock2 import data
+
 #Parametre pour l'affichage
 NOMBRE_DE_PAQUET_AFFICHE = 35
 MIN_CMAP = 0
 MAX_CMAP = 250
 TAILLE_POINTS = 5.0
-TAILLE = 3000
+TAILLE_FONT= 7
+TAILLE_RADAR = 1000
 
 class RadarTab:
     def __init__(self,tabControl):
@@ -43,17 +47,18 @@ class RadarTab:
     
         self.all_x,self.all_y, self.all_confiance = [],[],[]
         self.en_cours_execution = False
+        self.radar_size = TAILLE_RADAR
         #self.connexion_websocket = websockets.connect('ws://192.168.137.1:8000')
 
         self.fig= Figure()
         self.fig_copy = self.fig
         self.axe = self.fig.add_subplot(projection = 'polar')
-        self.axe.set_ylim(bottom=0,top=TAILLE)
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame_1) 
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.config(width=200,height=200)
         self.canvas_widget.pack(fill='both', expand=True,padx=10,pady=10)
+        self.update_display()
 
         # Create the colormap
         self.custom_cmap = colors.LinearSegmentedColormap('CustomCmap',{
@@ -73,6 +78,22 @@ class RadarTab:
         Button(self.frame_2_3, text="Désactiver", command= self.desactiver_radar).pack(fill="both",side="left",expand=True)
         Button(self.frame_2_3, text="Clear", command= self.clear_radar).pack(fill="both",side="left",expand=True)
         Button(self.frame_2_4,text="Enregistrer", command=self.enregistrer_image).pack(fill="both",side="top",expand=True)
+        Scale(self.frame_2_4,from_=1000,to=12000, orient=HORIZONTAL,resolution=1000,command=self.actualiser_radar_size,variable=self.radar_size,label="Echelle du radar:").pack(fill="both",side="bottom",expand=True)
+        self.axe.tick_params(axis='y', labelsize=TAILLE_FONT)
+
+# Actualiser la taille du radar en fonction du Scale
+    def actualiser_radar_size(self,valeur):
+        self.radar_size = int(valeur)
+        if(self.radar_size>6000):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 3000))
+        elif(self.radar_size>4000):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 2000))
+        elif(self.radar_size>0):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 1000))
+        else:
+            raise ValueError
+        self.axe.set_ylim(0,self.radar_size)
+        self.canvas.draw()
 
 # Enregistrer une image du radar
     def enregistrer_image(self):
@@ -80,6 +101,13 @@ class RadarTab:
         file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("Fichiers PNG", "*.png")])
         if not file_path:
             return
+        if(self.radar_size>6000):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 4000))
+        elif(self.radar_size>0):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 2000))
+        else:
+            raise ValueError
+        self.axe.tick_params(axis='y',labelsize=0)
         figure.savefig(file_path, format="png",dpi=500)
         print("Enregistrer")
 
@@ -89,7 +117,7 @@ class RadarTab:
         if self.en_cours_execution == True:
             self.axe.clear()
             for i in paquet:
-                self.label_1 .configure(text="La distance " +str(int(i[1])) +", l'angle " +str(int(i[0])))
+                self.label_1.configure(text="La distance " + str(int(i[0])) + ", l'angle " + str(int(i[1])))
                 self.all_x.append(np.radians(i[1]))
                 self.all_y.append(i[0])
                 self.all_confiance.append(i[2])
@@ -138,7 +166,22 @@ class RadarTab:
 
 #Update du display
     def update_display(self):
-        self.axe.set_ylim(0,TAILLE)
+        if(self.radar_size>6000):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 3000))
+        elif(self.radar_size>4000):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 2000))
+        elif(self.radar_size>0):
+            self.axe.set_rgrids(np.arange(0, self.radar_size, 1000))
+        else:
+            ValueError
+        self.axe.tick_params(axis='y', labelsize=TAILLE_FONT)
+        self.axe.set_ylim(0,self.radar_size)
         self.canvas.draw()
         self.frame_0.update()
 
+#test d'un mock pour visualisation de l'interface
+#La fonction test doit etre remplacer dans la fonction activer_radar
+#Elle doit remplacer a ligne "self.packet_thread()" par "self.test()"
+    # def test(self):
+    #     for i in data:
+    #         self.actualisation_radar(i["dataPoints"])
