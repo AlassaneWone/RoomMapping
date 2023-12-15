38787aff0309e6c30a2b08f4a51259ba4a247f0b
Bonheure Antoine <he202146@students.ephec.be>
2023-12-15 08:19:55 +0100

    Frame du Home

--- interface_operateur/tabs/homeTab.py
+++ interface_operateur/tabs/homeTab.py
@@ -1,13 +1,30 @@
 from tkinter import *
-from tkinter.ttk import *
+import tkinter.ttk as ttk
 from PIL import Image, ImageTk  # Utilisé pour afficher des images
 
 
 class HomeTab:
     def __init__(self, tabControl):
-        self.tab = Frame(tabControl)
-        tabControl.add(self.tab, text="Home")
 
-        self.label = Label(self.tab, text="It is the Home Page").pack()
+        self.frame_0 = Frame(master=tabControl,bg='white')
+        tabControl.add(self.frame_0, text="Home")
+
+        
+        self.frame_1 = Frame(self.frame_0,background= "#d1d1d1")
+        self.frame_2 = Frame(self.frame_0,background= "#d1d1d1" )
+
+        self.frame_2_1 = Frame(self.frame_2, bg='#FFFFFF')
+        self.frame_2_2 = Frame(self.frame_2,height=100, bg = '#FFFFFF')
+        self.frame_2_3 = Frame(self.frame_2,height=100, bg = '#FFFFFF')
+
+        self.frame_1.pack(side="left" ,fill="both",expand=TRUE,padx=10,pady=10)
+        self.frame_2.pack(side="left",fill="both",expand=TRUE,padx=10,pady=10)
+        self.frame_2_1.pack(side="top" ,fill="both",expand=TRUE,padx=10,pady=10)
+        self.frame_2_2.pack(side="top",fill="x",expand=False,padx=10)
+        self.frame_2_3.pack(side="bottom",fill="x",expand=False,padx=10,pady=[0,10])
+        
+        self.label_1 = Label(self.frame_1, text="Bonjour")
+        self.label_1.pack(padx=10,pady=10)
+        
 
 
