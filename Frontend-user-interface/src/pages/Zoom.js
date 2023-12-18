3868d27cb3855578c77bbc5ec9be54a740ab7524
Slywoud <he202160@students.ephec.be>
2023-12-18 17:32:26 +0100

    Style Update for the zoom functionality
    Centralized the Style in the file Zoom.css for cleaner code / avoid mixing react style and css.

--- Frontend-user-interface/src/pages/Zoom.js
+++ Frontend-user-interface/src/pages/Zoom.js
@@ -4,50 +4,31 @@ import "../styles/Zoom.css";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"; // Importez le fichier CSS de Bootstrap
 
-function Zoom({imageUrl}) {
+function Zoom({ imageUrl }) {
     // Image URL used for tests
-    //imageUrl = "https://roommappingbucket.s3.eu-north-1.amazonaws.com/de_mirage-map-callouts.jpg"
-
-    const Controls = () => {
-        const { zoomIn, zoomOut, resetTransform } = useControls();
-        return (
-            <div style={{ position: "absolute", bottom: "10px", left: "50%", transform: "translateX(-50%)" }}>
-                <Button variant="primary" onClick={() => zoomIn()}>
-                    Zoom In
-                </Button>{" "}
-                <Button variant="secondary" onClick={() => zoomOut()}>
-                    Zoom Out
-                </Button>{" "}
-                <Button variant="danger" onClick={() => resetTransform()}>
-                    Reset
-                </Button>
-            </div>
-        );
-    };
-
-    const containerStyle = {
-        position: "relative", // Ajout de cette ligne pour définir le positionnement relatif
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%"
-    };
-
-    const imageStyle = {
-        width: "100%",
-        height: "auto",
-        objectFit: "contain"
-    };
+    // imageUrl = "https://roommappingbucket.s3.eu-north-1.amazonaws.com/de_mirage-map-callouts.jpg"
 
     return (
-        <div className="Zoomzoom" style={containerStyle}>
+        <div className="Zoomzoom">
             <TransformWrapper defaultScale={1}>
-                <TransformComponent>
-                    <img src={imageUrl} alt="Image" style={imageStyle} />
-                </TransformComponent>
-                <Controls />
+                {({ zoomIn, zoomOut, resetTransform }) => (
+                    <>
+                        <TransformComponent>
+                            <img src={imageUrl} alt="Image" />
+                        </TransformComponent>
+                        <div className="controls">
+                            <Button variant="primary" onClick={() => zoomIn()}>
+                                Zoom In
+                            </Button>{" "}
+                            <Button variant="secondary" onClick={() => zoomOut()}>
+                                Zoom Out
+                            </Button>{" "}
+                            <Button variant="danger" onClick={() => resetTransform()}>
+                                Reset
+                            </Button>
+                        </div>
+                    </>
+                )}
             </TransformWrapper>
         </div>
     );
